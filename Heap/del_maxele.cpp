25361f95a0a3b892da3c4e18250d55ecc0ad9a07
Arun <arunmadhopur750@gmail.com>
2021-12-01 11:28:06 +0530

    Heap added

---
+++ Heap/del_maxele.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+void heapify(vector<int>&arr, int index, int size)
+{
+    int left, right, max;
+    left = 2*index + 1;
+    right = left + 1;
+    max = index;
+
+    if (left <= size && arr[left] > arr[max])
+        max = left;
+
+    if (right <= size && arr[right] > arr[max])
+        max = right;
+
+    if (index!=max)
+    {
+        swap(arr[max], arr[index]);
+        heapify(arr, max, size);
+    }
+}
+void buildheap(vector<int> &arr, int size)
+{
+    for (int i = size / 2; i >= 0; i--)
+    {
+        heapify(arr, i, size);
+    }
+}
+void DeleteMax(vector<int> &arr, int index, int size)
+{
+    swap(arr[0], arr[size]);
+    // cout << "Max ele : " << arr[size];
+    size = size - 1;
+    heapify(arr, 0, size);
+}
+int main()
+{
+    vector<int> arr = {10, 20, 40, 30, 80, 60, 50};
+    buildheap(arr, arr.size() - 1);
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
+    DeleteMax(arr,0,arr.size()-1);
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
