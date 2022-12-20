a7f15f6cdf052b7eb8889805b8192e1abd931368
Arun <arunmadhopur750@gmail.com>
2022-12-21 00:51:16 +0530

    files added in heap

---
+++ 7_Heap/9_recognizestring.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+string reorganizeString(string s)
+{
+    unordered_map<char, int> um;
+    for (auto ele : s)
+    {
+        um[ele]++;
+    }
+    priority_queue<pair<int, char>> pq;
+
+    for (auto ele : um)
+    {
+        pq.push({ele.second, ele.first});
+    }
+    string ans = "";
+    pair<int, char> block = {-1, '&'};
+    while (pq.size() > 0)
+    {
+        auto top = pq.top();
+        pq.pop();
+        ans += top.second;
+        top.first--;
+
+        if (block.first > 0)
+            pq.push(block);
+        block = top;
+    }
+    if (ans.length() == s.length())
+        return ans;
+    return "";
+}
+int main()
+{
+}
